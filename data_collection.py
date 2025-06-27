bd48b5f04845878f84d673aa3b955ee275824952
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-06-27 16:24:35 +0000

    modules to use

---
+++ data_collection.py
@@ -0,0 +1,8 @@
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
