154d65535944c0cec6518b483194cf5c8d790405
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-06-29 00:48:32 +0000

    instantiate both classes and perform operations in the main file

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-import data_collection
+from data_collection import DataCollection
 from data_to_sheet import UploadData
+
+data_collection = DataCollection()
+upload_data = UploadData()
+
+try:
+    for i in range(len(data_collection.property_addresses)):
+
+        # get data and fill form automatically
+        upload_data.house_data_upload(
+            data_collection.property_addresses[i],
+            data_collection.price_list[i],
+            data_collection.address_links[i],
+        )
+except Exception as e:
+    print('exception:', e)
