4237a98d942e1b5ca3f376af539f3eefc3db9b57
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-06-29 00:09:29 +0000

    collect data from zillow clone using beautisoup

--- data_collection.py
+++ data_collection.py
@@ -1,8 +1,53 @@
-
+import lxml
+import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver import ChromeOptions
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+
+ZILLOW_CLONE = 'https://appbrewery.github.io/Zillow-Clone/'
+
+# get zillow html
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:115.0) Gecko/20100101 Firefox/115.0'
+}
+zillow_web = requests.get(ZILLOW_CLONE, headers=header)
+zillow_web.raise_for_status()
+zillow_html = zillow_web.text
+
+entry_soup = BeautifulSoup(zillow_html, 'lxml')
+
+price_list = []
+try:
+    # rent prices
+    for span in entry_soup.find_all(
+            name='span', class_='PropertyCardWrapper__StyledPriceLine'):
+        if '+' in span.getText():
+            price = round(float(
+                (span.getText()
+                 ).split('$')[1].split('+')[0].replace(',', '')
+            )
+            )
+            price_list.append(price)
+        else:
+            price = round(float(
+                (span.getText()
+                 ).split('$')[1].split('/')[0].replace(',', '')
+            )
+            )
+            price_list.append(price)
+
+except ValueError as ve:
+    print('exception:', ve)
+
+
+# property address links
+address_links = [
+    anchor.get('href') for anchor in entry_soup.find_all(
+        name='a', class_='property-card-link'
+    )
+]
+
+# property addresses
+property_addresses = [
+    address.getText().strip() for address in entry_soup.select(
+        'a.StyledPropertyCardDataArea-anchor address'
+    )
+]
